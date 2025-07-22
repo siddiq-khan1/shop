0e810223968905b329f25e48fed54a11303b2d68
siddiq-khna1 <siddiquealiofficail@gamil.com>
2025-07-22 23:05:39 +0500

    add

---
+++ script.js
@@ -0,0 +1,13 @@
+let name ="5";
+console.log("name");
+console.log("bravo ");
+let a=3;
+let b = 5;
+console.log("A + B", a+b);
+let multiple;
+multiple = prompt("number?");
+if (multiple % 5 === 0){
+    console.log(multiple "is a multiple of 5");
+}else {
+    console.log(multiple "is not a multiple 5");
+}
